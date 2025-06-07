8be829033229a9f23c5d388d2e6267dd1cfabfed
BayMx19 <isaimanmuhammad1@gmail.com>
2025-06-07 14:31:37 +0700

    Migration Update for Users Pembeli

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,14 +19,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role');
-            $table->text('alamat')->nullable();
-            $table->text('provinsi')->nullable();
-            $table->text('kota')->nullable();
-            $table->text('kecamatan')->nullable();
-            $table->text('kelurahan')->nullable();
-            $table->text('RT', 3)->nullable();
-            $table->text('RW', 3)->nullable();
-            $table->string('kode_pos')->nullable();
             $table->string('foto_profil')->nullable();
             $table->string('jenis_kelamin')->nullable();
             $table->string('status')->default('ACTIVE')->nullable();
